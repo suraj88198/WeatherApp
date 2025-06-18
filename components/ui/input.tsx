ca1d5cad5ff19d641da9c77b4cc6fff1bf1c3499
Suraj <suraj.kushwah@think.tank>
2025-06-18 15:54:15 +0530

    input changes

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -6,13 +6,7 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
     return (
-      <input
-        ref={ref}
-        className={cn(
-          "px-4 py-2 rounded-full bg-white text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-yellow-400",
-          className
-        )}
-        {...props}
+      <input ref={ref} className={cn("px-4 py-2 rounded-full bg-white text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-yellow-400",)}{...props}
       />
     );
   }
