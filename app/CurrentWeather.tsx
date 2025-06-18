5945e3fbb8a87b67eb1caec1b036e856270180e4
Suraj <suraj.kushwah@think.tank>
2025-06-18 14:28:31 +0530

    css update

--- app/CurrentWeather.tsx
+++ app/CurrentWeather.tsx
@@ -3,7 +3,7 @@ export function CurrentWeather({ data }: { data: any }) {
   if (!data) return null;
   const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   return (
-    <div className="bg-white/20 dark:bg-zinc-800/50 shadow-xl rounded-2xl p-6 text-left backdrop-blur-md flex items-center justify-between flex-wrap">
+    <div className="bg-white/20 dark:bg-zinc-800/50 shadow-xl rounded-2xl p-6 text-left backdrop-blur-md flex items-center md:justify-between flex-wrap justify-center">
       <div>
       <h2 className="text-3xl font-bold mb-4 text-yellow-300">Current Weather</h2>
       <p className="text-xl font-medium mb-2">City: <span className="text-white">{data.name}</span></p>
